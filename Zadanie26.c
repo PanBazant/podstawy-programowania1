d7d7debc45504907d8a7f75bd8a1400bcbb85fb6
PanBazant <32612536+PanBazant@users.noreply.github.com>
2017-10-13 17:27:29 +0200

    Add files via upload

---
+++ Zadanie26.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int a,b;
+    
+    for(a=1;a<=10;a++)
+    {
+        for(b=1;b<=10;b++)
+        {printf("%d ",a*b);
+        }
+        printf("\n");
+}
+    return 0;
+}
