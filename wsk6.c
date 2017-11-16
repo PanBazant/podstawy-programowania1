825a3f183dc459e5cce8c871e5a6e4ee3e733b7e
PanBazant <32612536+PanBazant@users.noreply.github.com>
2017-11-16 19:33:22 +0100

    Add files via upload

---
+++ wsk6.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    float *tab = malloc(n* sizeof(float *));
+    int i;
+    for(i=0;i<n;i++) {
+        scanf("%f",*(tab+i));
+    }
+    float suma=0;
+    for(i=0;i<n;i++){
+        suma= suma+ *(tab+1);
+    }
+    printf("%f",suma);
+    
+    
+    
+    return 0;
+}
